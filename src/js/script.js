cc9633acbf20a590869a25dccebbacf90471a3fe
deVolpe <deVolpe@users.noreply.github.com>
2019-07-13 01:52:00 +0300

    part 1

---
+++ src/js/script.js
@@ -0,0 +1,34 @@
+const data = {};
+
+var events = JSON.parse(localStorage.getItem('events')) || [];
+
+// Local data
+data.local = {};
+data.local.week = [
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+  'Sunday'
+];
+data.local.months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
+];
+data.local.monthsList = function() {
+  return this.months.map(month => month.toLowerCase());
+};
+
+// window.onload = () => {};
