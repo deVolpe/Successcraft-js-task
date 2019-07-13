f6f4714c87379d4c3daa0eaee7602cbfad0bc8be
deVolpe <deVolpe@users.noreply.github.com>
2019-07-14 02:26:28 +0300

    part 2

--- src/js/script.js
+++ src/js/script.js
@@ -30,5 +30,24 @@ data.local.months = [
 data.local.monthsList = function() {
   return this.months.map(month => month.toLowerCase());
 };
+data.currentDate = {
+  month: new Date().getMonth(),
+  year: new Date().getFullYear()
+};
 
-// window.onload = () => {};
+const createElement = (el, cls, txt = null) => {
+  const element = document.createElement(el);
+  if (txt) element.textContent = txt;
+  if (cls) element.className = cls;
+  return element;
+};
+
+window.onload = () => {
+  const currentDate = document.getElementById('current');
+  currentDate.textContent = `${data.local.months[data.currentDate.month]} ${
+    data.currentDate.year
+  }`;
+  document.getElementById('add').addEventListener('mousedown', e => {
+    const addForm = createElement('div', null, 'add-form');
+  });
+};
